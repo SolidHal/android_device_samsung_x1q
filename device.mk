f4941383ff94fc672687d07400cecf10ade186f5
SolidHal <hal@halemmerich.com>
2021-06-01 22:48:10 +0000

    cleanup, don't need seperate lineage makefiles
    Change-Id: Ibfd7f7c9543634c093fc8d0043a31ebf8d4f8bb4

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@ $(call inherit-product, device/samsung/sm8250-common/sm8250.mk)
 # Proprietary blobs
 $(call inherit-product-if-exists, vendor/samsung/x1q/x1q-vendor.mk)
 
+### DALVIK
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 DEVICE_PATH := device/samsung/x1q
 
 # Soong Namespaces
@@ -12,16 +15,15 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Overlays
 PRODUCT_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay
+PRODUCT_PACKAGE_OVERLAYS += $(DEVICE_PATH)/overlay-lineage
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(DEVICE_PATH)/overlay-lineage/lineage-sdk
 
 # Resolution
 TARGET_SCREEN_HEIGHT := 3200
 TARGET_SCREEN_WIDTH := 1440
 
+# Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := large
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
 PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
-# Lineage
-ifneq ($(LINEAGE_BUILD),)
--include $(DEVICE_PATH)/device_lineage.mk
-endif
--- lineage_x1q.mk
+++ lineage_x1q.mk
@@ -1,8 +1,21 @@
-# Inherit full device configuration
-$(call inherit-product, device/samsung/x1q/full_x1q.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/x1q/device.mk)
 
 # Inherit some common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+
 # Product IDs
 PRODUCT_NAME := lineage_x1q
+PRODUCT_DEVICE := x1q
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-G9810
+
+PRODUCT_CHARACTERISTICS := phone
+PRODUCT_SHIPPING_API_LEVEL := 29
+PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
